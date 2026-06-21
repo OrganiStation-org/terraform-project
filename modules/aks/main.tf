b7f158dc45db336a659b31fbcf15c9acf35e4b0a
Mahesh Dineshkumar(UST <307420@ust.com>
2026-06-22 00:21:51 +0530

    aks conf error

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -23,8 +23,10 @@ resource "azurerm_kubernetes_cluster" "this" {
   }
 
   network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
+    network_plugin     = "azure"
+    load_balancer_sku  = "standard"
+    service_cidr       = "172.16.0.0/16"
+    dns_service_ip     = "172.16.0.10"
   }
 
   oidc_issuer_enabled       = true
