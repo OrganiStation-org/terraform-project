08277ffd98acb71655014470f04b83f7465560d4
Mahesh Dineshkumar(UST <307420@ust.com>
2026-06-22 13:00:35 +0530

    Updated the provider version

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -14,23 +14,3 @@ resource "azurerm_monitor_workspace" "this" {
   location            = var.location
   tags                = var.tags
 }
-
-# Azure Managed Grafana
-resource "azurerm_dashboard_grafana" "this" {
-  name                              = "${var.project_name}-grafana-${var.env}"
-  resource_group_name               = var.resource_group_name
-  location                          = var.location
-  api_key_enabled                   = true
-  deterministic_outbound_ip_enabled = false
-  public_network_access_enabled     = true
-  grafana_major_version             = 12
-  tags                              = var.tags
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  azure_monitor_workspace_integrations {
-    resource_id = azurerm_monitor_workspace.this.id
-  }
-}
